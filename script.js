123e987e48fdb38d1546efb9f4aa917885be7767
Daniel <54555281+Danielthemouse@users.noreply.github.com>
2020-01-15 19:03:30 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -1,7 +1,5 @@
 var stat = 0;
 
-var buttons = '<input type="submit" value="Suicide" onclick="stat = 2;" /><input type="submit" value="S T O P" onclick="stat = 3;" />';
-
 var element = document.getElementById("Starter");
 
 function updater() {
@@ -9,12 +7,16 @@ function updater() {
         element.innerHTML = ""
     }
     else if (stat == 1) {
-        element.innerHTML = "You're a very sad hamus... </br>You think about commiting a suicide.. </br>What should you do?</br>" + buttons;
+        element.innerHTML = "You're a very sad hamus... </br>You think about commiting a suicide.. </br>What should you do?</br>" + '<input class ="btn" type="submit" value="Suicide" onclick="stat = 2;" /><input class ="btn" type="submit" value="S T O P" onclick="stat = 3;" />';
     }
     else if (stat == 2) {
         element.innerHTML = "GAME OVER </br>"
         stat = 0
     }
+    else if (stat == 3) {
+        element.innerHTML = "You need to go to school.. FUCK school... </br>Are you sure about not suiciding?.. </br>What should you do?</br>" + '<input class ="btn" type="submit" value="Suicide" onclick="stat = 2;" /><input class ="btn" type="submit" value="S T O P" onclick="stat = 3;" />';
+        stat = 0
+    }
 }
 
-setInterval(updater, 2500);
+setInterval(updater, 1500);
