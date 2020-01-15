f8cc6cb2e0d863718c943ee48f0af68465c72ba9
Daniel <54555281+Danielthemouse@users.noreply.github.com>
2020-01-15 19:17:17 +0200

    Update script.js

--- script.js
+++ script.js
@@ -15,7 +15,6 @@ function updater() {
     }
     else if (stat == 3) {
         element.innerHTML = "You need to go to school.. FUCK school... </br>Are you sure about not suiciding?.. </br>What should you do?</br>" + '<input class ="btn" type="submit" value="Suicide" onclick="stat = 2;" /><input class ="btn" type="submit" value="S T O P" onclick="stat = 3;" />';
-        stat = 0
     }
 }
 
