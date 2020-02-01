0f90d9169f88f46378cd901a64928344e616f47f
Danielthemouse <danielharush5252@gmail.com>
2020-02-01 11:30:35 +0200

    Format

--- script.js
+++ script.js
@@ -6,14 +6,16 @@ function updater() {
     if (stat == 0) {
         element.innerHTML = "<b>PRESS START</b>"
     } else if (stat == 1) {
-        element.innerHTML = "You're a very sad hamus... </br>You think about commiting a suicide.. </br><b>What should you do?<b></br>" + '<input class ="btn" type="submit" value="Suicide" onclick="stat = 2;" /><input class ="btn" type="submit" value="S T O P" onclick="stat = 3;" />';
+        element.innerHTML = "You're a very sad hamus... </br>You think about commiting a suicide.. </br><b>What should you do?<b></br>" +
+         '<input class ="btn" type="submit" value="Suicide" onclick="stat = 2;" /><input class ="btn" type="submit" value="S T O P" onclick="stat = 3;" />';
     } else if (stat == 2) {
         element.innerHTML = "GAME OVER </br>"
         stat = 0
     } else if (stat == 3) {
-        element.innerHTML = "You need to go to school.. FUCK school... </br>Are you sure about not suiciding?.. </br><b>What should you do?<b></br>" + '<input class ="btn" type="submit" value="Suicide" onclick="stat = 2;" /><input class ="btn" type="submit" value="S T O P   I SAID   S T O P" onclick="stat = 4;" />';
+        element.innerHTML = "You need to go to school.. FUCK school... </br>Are you sure about not suiciding?.. </br><b>What should you do?<b></br>" +
+         '<input class ="btn" type="submit" value="Suicide" onclick="stat = 2;" /><input class ="btn" type="submit" value="S T O P   I SAID   S T O P" onclick="stat = 4;" />';
     } else if (stat == 4) {
-        element.innerHTML = "You're going to sleep.</br>You fell down the stairs while getting to bed...</br>";
+        element.innerHTML = "You're going to sleep.</br>You woke up.</br>What should you do?";
         stat = 2;
     }
 }
