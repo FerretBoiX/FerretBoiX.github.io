0132f963e41e88764c8e5c988453cecd306f4dce
Danielthemouse <danielharush5252@gmail.com>
2020-02-01 12:09:57 +0200

    Add options and description

--- script.js
+++ script.js
@@ -15,8 +15,11 @@ function updater() {
         element.innerHTML = "You need to go to school.. FUCK school... </br>Are you sure about not suiciding?.. </br><b>What should you do?<b></br>" +
          '<input class ="btn" type="submit" value="Suicide" onclick="stat = 2;" /><input class ="btn" type="submit" value="S T O P   I SAID   S T O P" onclick="stat = 4;" />';
     } else if (stat == 4) {
-        element.innerHTML = "You're going to sleep.</br>You woke up.</br>What should you do?";
-        stat = 2;
+        element.innerHTML = "You're going to sleep.</br>You woke up.</br>What should you do?</br>" +
+        '<input class ="btn" type="submit" value="Get ready for school" onclick="stat = 5;" /><input class ="btn" type="submit" value="Waste all of your time watching memes" onclick="stat = 6;" />';
+    } else if (stat == 5) {
+        element.innerHTML = "You got ready for school.</br>You still think about suiciding..</br>What should you do?" +
+        '<input class ="btn" type="submit" value="Just do it already" onclick="stat = 2;" /><input class ="btn" type="submit" value="Stop it. Go to school. Be a normal person." onclick="stat = 6;" />';
     }
 }
 
