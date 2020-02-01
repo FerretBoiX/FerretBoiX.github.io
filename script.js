f63a8aa59da4e90c7626578d6d95e2d5621b82f8
Danielthemouse <danielharush5252@gmail.com>
2020-02-01 11:19:39 +0200

    Format

--- script.js
+++ script.js
@@ -5,18 +5,14 @@ var element = document.getElementById("Starter");
 function updater() {
     if (stat == 0) {
         element.innerHTML = "<b>PRESS START</b>"
-    }
-    else if (stat == 1) {
+    } else if (stat == 1) {
         element.innerHTML = "You're a very sad hamus... </br>You think about commiting a suicide.. </br><b>What should you do?<b></br>" + '<input class ="btn" type="submit" value="Suicide" onclick="stat = 2;" /><input class ="btn" type="submit" value="S T O P" onclick="stat = 3;" />';
-    }
-    else if (stat == 2) {
+    } else if (stat == 2) {
         element.innerHTML = "GAME OVER </br>"
         stat = 0
-    }
-    else if (stat == 3) {
+    } else if (stat == 3) {
         element.innerHTML = "You need to go to school.. FUCK school... </br>Are you sure about not suiciding?.. </br><b>What should you do?<b></br>" + '<input class ="btn" type="submit" value="Suicide" onclick="stat = 2;" /><input class ="btn" type="submit" value="S T O P   I SAID   S T O P" onclick="stat = 4;" />';
-    }
-    else if (stat == 4) {
+    } else if (stat == 4) {
         element.innerHTML = "You're going to sleep.</br>You fell down the stairs while getting to bed...</br>";
         stat = 2;
     }
