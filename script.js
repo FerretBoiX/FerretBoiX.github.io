f66cf3b330ccca34e80adaf0fb3223692333402e
Danielthemouse <danielharush5252@gmail.com>
2020-02-01 16:41:21 +0200

    Added a situation

--- script.js
+++ script.js
@@ -11,6 +11,9 @@ function updater() {
     } else if (stat == 2) {
         element.innerHTML = "You are the class's quiet kid and you are being bullied</br><b>What should you do?<b></br>" +
          '<input class ="btn" type="submit" value="Suicide" onclick="stat = 2.1;" /><input class ="btn" type="submit" value="Tell the teacher" onclick="stat = 2.2;" /><input class ="btn" type="submit" value="Fight back" onclick="stat = 2.3;" />';
+    } else if (stat == 3) {
+        element.innerHTML = "You were just told your girlfriend cheated on you..</br><b>What should you do?<b></br>" +
+         '<input class ="btn" type="submit" value="Break up with her" onclick="stat = 3.1;" /><input class ="btn" type="submit" value="Ignore the fact and pretend it didn\'t happen" onclick="stat = 3.2;" /><input class ="btn" type="submit" value="Murder her" onclick="stat = 3.3;" />';
     } 
 }
 
--- script2.js
+++ script2.js
@@ -10,6 +10,15 @@ function updater2() {
     }  else if (stat == 2.3) {
         element.innerHTML = "You fought back!</br>One of the teachers caught you and you were kicked out of school.";
         stat = 1;
+    } else if (stat == 3.1) {
+        element.innerHTML = "You just broke up with her and continued your life.</br>Maybe you'll find a better girlfriend one day.";
+        stat = 4;
+    } else if (stat == 3.2) {
+        element.innerHTML = "You just ignore the fact.</br>One day she asks you when will you marry her.</br>Tears drop and you have a mental breakdown.</br>You can't do it anymore so you die.";
+        stat = 1;
+    }  else if (stat == 3.3) {
+        element.innerHTML = "You murdered her.</br>Police found you were the one who killed her.</br>What were you expecting would happen?!.";
+        stat = 1;
     }
 }
 
