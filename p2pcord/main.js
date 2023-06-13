2a8d0973b4cfeef2a76a8fe6d072a77c79f13de1
Ferret <54555281+FerretBoiX@users.noreply.github.com>
2023-06-14 02:42:49 +0300

    Add files via upload

---
+++ p2pcord/main.js
@@ -0,0 +1,46 @@
+var peer = new Peer(Math.random().toString(16).slice(8));
+
+peer.on('open', function(id) {
+	document.getElementById('youridh').innerHTML = `<strong>Your ID: </strong> ${id}`;
+  });
+
+document.getElementById('conButt').onclick = function() {
+    if(document.getElementById('conID').value != null) {
+        var conn = peer.connect(document.getElementById('conID').value);
+        navigator.mediaDevices
+          .getUserMedia({
+            audio: true,
+            video: false,
+          })
+          .then((stream) => {
+            var call = peer.call(document.getElementById('conID').value, stream);
+            const video = document.querySelector("video");
+            video.srcObject = stream;
+            video.onloadedmetadata = () => {
+              video.play();
+            };
+          })
+          .catch((err) => {
+           console.log(err)
+          });
+    }
+}
+
+peer.on('call', function(call) {
+	navigator.mediaDevices
+          .getUserMedia({
+            audio: true,
+            video: false,
+          })
+          .then((stream) => {
+            call.answer(stream);
+            const video = document.querySelector("video");
+            video.srcObject = stream;
+            video.onloadedmetadata = () => {
+              video.play();
+            };
+          })
+          .catch((err) => {
+           console.log(err)
+          });
+  });
